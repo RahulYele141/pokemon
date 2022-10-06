ef7f699149b9d0be489717efdce91447072bdb40
RahulYele141 <raytheperfect@gmail.com>
2022-10-06 22:09:51 +0530

    working with API

--- src/component/home/home.component.jsx
+++ src/component/home/home.component.jsx
@@ -1,55 +1,53 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-const API_URL = 'https://pokeapi.co/api/v2/pokemon'
+const API_URL = "https://pokeapi.co/api/v2/pokemon";
 
 const Home = () => {
-    const [pokemonList, setPokemonList] = useState([])
-    const [pokemonInfo, setPokemonInfo] = useState([])
+  //   const [pokemonList, setPokemonList] = useState([]);
+  const [pokemonInfo, setPokemonInfo] = useState([]);
 
-    useEffect(() => {
-        fetch(`${API_URL}?limit=20`).then(response => {
-            return response.json()
-        }).then(response => {
-            // response.results
-            const pokemon = response.results.map((url) => {
-                console.log(url);
-                return url.url
-            })
-            setPokemonInfo(pokemon)
-            setPokemonList(response.results)
-        })
-    }, [])
-    console.log(pokemonInfo);
+  useEffect(() => {
+    fetch(`${API_URL}?limit=10`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        const pokemon = response.results.map((url) => {
+          fetch(`${url.url}`)
+            .then((response) => response.json())
+            .then((response) => {
+              console.log(response);
+            });
+        });
+      });
+  }, []);
 
-    const fetchPokemon = (index) => {
-        console.log(index);
-        fetch(`${API_URL}/${index}`).then(response => response.json()).then(response => {
-            console.log(response);
+  console.log("here", pokemonInfo);
 
-        })
-    }
+  return (
+    <div>
+      {pokemonInfo.map((pokemon, key) => {
+        return (
+          <div
+            key={key}
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <div style={{ padding: "10px" }}>
+              <h1>
+                {key + 1} {pokemon.name}
+              </h1>
+            </div>{" "}
+            <div style={{ padding: "10px" }}>
+              <img src={`${pokemon.sprites.other.dream_world.front_default}`} />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-    return (
-        <div>{pokemonList.map((pokemon, key) => {
-            return (
-                <div key={key} style={{
-                    display: 'flex', flexDirection: 'row'
-                }}>
-                    <div style={{ padding: '10px' }}>
-                        <h1>
-                            {key + 1} {" "} {pokemon.name}
-                        </h1>
-                    </div>
-                    {" "}
-                    <div style={{ padding: '10px' }}>
-                        <h1 onClick={() => fetchPokemon(key + 1)} href=''>
-                            {pokemon.url}
-                        </h1>
-                    </div>
-                </div>
-            )
-        })}</div >
-    )
-}
-
-export default Home
+export default Home;
