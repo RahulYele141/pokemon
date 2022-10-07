2fb735f2c097e6a40e740847528b68bd7ea7f44a
RahulYele141 <raytheperfect@gmail.com>
2022-10-07 19:03:26 +0530

    reverted

--- src/component/constants/api.jsx
+++ src/component/constants/api.jsx
@@ -1,8 +1,10 @@
 const API_POKEMON = 'https://pokeapi.co/api/v2/pokemon';
 const API_TYPES = 'https://pokeapi.co/api/v2/type'
 
+const colors = ['#DDCBD0', '#FCC1B0', '#B2D2E8', '#CFB7ED', '#F4D1A6', '#C5AEA8', '#C1E0C8', '#D7C2D7', '#C2D4CE', '#EDC2C4', '#CBD5ED', '#COD4C8', '#E2E2A0', '#DDCOCF', '#C7D7DF', '#CADCDF', '#C6C5E3', '#E4C0CF', '#CODFDD', '#CACACA']
+
 export const fetchPokemonData = async () => {
-    const response = await fetch(`${API_POKEMON}?limit=04`).then((response) => response.json());
+    const response = await fetch(`${API_POKEMON}?limit=03`).then((response) => response.json());
     const json = await response.results;
 
     const promises = await json.map(url => {
@@ -19,8 +21,15 @@ export const fetchPokemonData = async () => {
 export const fetchPokemonTypes = async () => {
     const response = await fetch(`${API_TYPES}`).then(response => response.json())
     const json = await response.results
-    const types = json.map((type) => {
-        return type.name
+    const types = json.map((type, ind) => {
+        const name = type.name
+        return name
     })
-    return types
+    const values = types.map((type, ind) => {
+        let abc = {}
+        abc[`${type}`] = colors[ind]
+        return abc
+    })
+    console.log(values);
+    return values
 }
--- src/component/home/home.component.jsx
+++ src/component/home/home.component.jsx
@@ -3,7 +3,7 @@ import { fetchPokemonData, fetchPokemonTypes } from "../constants/api";
 import Card from "../pokecard/card.component";
 import './home.style.css'
 
-const colors = ['#DDCBD0', '#FCC1B0', '#B2D2E8', '#CFB7ED', '#F4D1A6', '#C5AEA8', '#C1E0C8', '#D7C2D7', '#C2D4CE', '#EDC2C4', '#CBD5ED', '#C0D4C8', '#E2E2A0', '#DDC0CF', '#C7D7DF', '#CADCDF', '#C6C5E3', '#E4C0CF', '#CODFDD', '#CACACA']
+//const colors = ['#DDCBD0', '#FCC1B0', '#B2D2E8', '#CFB7ED', '#F4D1A6', '#C5AEA8', '#C1E0C8', '#D7C2D7', '#C2D4CE', '#EDC2C4', '#CBD5ED', '#C0D4C8', '#E2E2A0', '#DDC0CF', '#C7D7DF', '#CADCDF', '#C6C5E3', '#E4C0CF', '#CODFDD', '#CACACA']
 
 const Home = () => {
     const [pokemons, setPokemons] = useState([])
@@ -20,30 +20,20 @@ const Home = () => {
     }, []);
 
 
-    console.log(types, colors.length);
+    console.log(types);
     return (
         <div className="home">
             {pokemons.map((pokemon, key) => {
-                let color1 = ''
-                let color2 = ''
 
-                console.log(pokemon.types)
-                types.filter((type, ind) => {
-                    if (pokemon.types[0].type.name === type) {
-                        color1 = colors[ind]
-                        return color1
-                    }
-
-                    if (pokemon.types[1].type.name && (pokemon.types[1].type.name === type)) {
-                        color2 = colors[ind]
-                        console.log(pokemon.types[0].type.name, types[ind], colors[ind], color2)
-                        return color2
-                    }
-                })
+                console.log(types[`${pokemon.types[0].type.name}`])
+                console.log(types['grass'])
+                console.log(pokemon.types[0].type.name)
+                let color1 = types[pokemon.types[0].type.name]
+                let color2 = types[pokemon.types[0].type.name]
 
                 return (
                     <div className="cards" key={key}>
-                        <Card color1={`${color1}`} color2={`${color2 ? color2 : color1}`} img={`${pokemon.sprites.other.dream_world.front_default}`} pokemon={pokemon.name} index={key} ></Card>
+                        <Card color1={`${color1}`} color2={`${color2}`} img={`${pokemon.sprites.other.dream_world.front_default}`} pokemon={pokemon.name} index={key} ></Card>
                     </div>
                 )
             })}
